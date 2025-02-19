180e242daae0848fe24a1508b8099b933041e20b
Aldrie <aldriemizobuti@outlook.com>
2025-02-19 19:23:26 -0300

    ✏️ fixes bad typings

--- packages/core/src/types/core.ts
+++ packages/core/src/types/core.ts
@@ -1,5 +1,5 @@
 import { ValueTypes } from './enums/ValueTypes';
 import { ConvertValuesToObject, Value } from './value';
 
-export type UnknowEnum = Record<string, string | number>;
+export type UnknowEnum = string | number | Record<string, string | number>;
 export type ExecutedNodeOutputs = Map<string, ConvertValuesToObject<Value<string, ValueTypes>>>;
